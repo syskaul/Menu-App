2aa9d24d38f1381c0e071bfaccd46223c1383613
Eshaan Kaul <eshaankaul29@gmail.com>
2020-07-19 20:54:41 +0530

    no message

--- functions/airtable/airtable.js
+++ functions/airtable/airtable.js
@@ -1,32 +1,40 @@
-const Airtable = require('airtable');
+const Airtable = require('airtable')
 
-/** THIS IS YOUR SERVERLESS FUNCTION */
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_KEY
+})
+const base = Airtable.base('appVLK6jIQQzfRDVa')
+
 exports.handler = function(event, context, callback) {
-  //pull the required information from your environment variables, which can be set in the Netlify UI
-  const {API_URL, API_CLIENT_ID, API_KEY } = process.env;
-
-  // THIS FUNCTION FORMATS AND SENDS YOUR RESPONSE BACK TO YOUR FRONT-END
-  const send = body => {
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(body)
-    });
-  }
-
-  // CONFIGURE YOUR AIRTABLE BASE CONNECTION
-  Airtable.configure({
-    endpointUrl: API_URL,
-    apiKey: API_KEY
-  });
-  var base = Airtable.base(API_CLIENT_ID);
-  
-  const data = [];
-  
-  /**
-    AIRTABLE REQUEST LOGIC GOES HERE, APPENDING TO DATA
-    REFERENCE YOUR BASE-SPECIFIC API FOR EXAMPLES OF
-    COMMON CRUD OPERATIONS
-  */
-
-  send(data);
+  const allRecords = []
+  base('entries')
+    .select({
+      maxRecords: 100,
+      view: 'all'
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+          allRecords.push(record)
+        })
+        fetchNextPage()
+      },
+      function done(err) {
+        if (err) {
+          callback(err)
+        } else {
+          const body = JSON.stringify({ records: allRecords })
+          const response = {
+            statusCode: 200,
+            body: body,
+            headers: {
+              'content-type': 'application/json',
+              'cache-control': 'Cache-Control: max-age=300, public'
+            }
+          }
+          callback(null, response)
+        }
+      }
+    )
 }
