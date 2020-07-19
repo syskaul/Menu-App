036f093d7d772e4affba2b307dd0e84374892925
Eshaan Kaul <eshaankaul29@gmail.com>
2020-07-19 19:52:17 +0530

    no message

--- functions/airtable/airtable.js
+++ functions/airtable/airtable.js
@@ -1,15 +1,32 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
+const Airtable = require('airtable');
+
+/** THIS IS YOUR SERVERLESS FUNCTION */
+exports.handler = function(event, context, callback) {
+  //pull the required information from your environment variables, which can be set in the Netlify UI
+  const {API_URL, API_CLIENT_ID, API_KEY } = process.env;
+
+  // THIS FUNCTION FORMATS AND SENDS YOUR RESPONSE BACK TO YOUR FRONT-END
+  const send = body => {
+    callback(null, {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+      body: JSON.stringify(body)
+    });
   }
+
+  // CONFIGURE YOUR AIRTABLE BASE CONNECTION
+  Airtable.configure({
+    endpointUrl: API_URL,
+    apiKey: API_KEY
+  });
+  var base = Airtable.base(API_CLIENT_ID);
+  
+  const data = [];
+  
+  /**
+    AIRTABLE REQUEST LOGIC GOES HERE, APPENDING TO DATA
+    REFERENCE YOUR BASE-SPECIFIC API FOR EXAMPLES OF
+    COMMON CRUD OPERATIONS
+  */
+
+  send(data);
 }
